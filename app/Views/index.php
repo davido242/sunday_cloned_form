8ec43db87a90b15b9755a736719d8fe2c3892117
David042 <Davidsarka242@gmail.com>
2021-06-21 14:04:40 -0700

    Some house cleaning

--- app/Views/index.php
+++ app/Views/index.php
@@ -2,24 +2,108 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Sunday Cloned Form</title>
+  <title>Sunday Clonning</title>
   <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css');?>">
-  <style>
-    *{
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-    body{
-      background-color: navy;
-    }
-  </style>
 </head>
 <body>
-  <h2>Hello</h2>
+  <div class="container-wrap">
+    <?php echo "Test Running Flex-Box shiit"?>
+    <h2 id="give"></h2>
+    <div class="first-child">
+      <div class="wrap-2">
+        <div class="Snd-child">1</div>
+        <div class="Snd-child">2</div>
+        <div class="Snd-child">3</div>
+        <div class="Snd-child">4</div>
+        <div class="Snd-child">5</div>
+        <div class="Snd-child">6</div>
+        <div class="Snd-child">7</div>
+        <div class="Snd-child">8</div>
+        <div class="Snd-child">9</div>
+        <div class="Snd-child">10</div>
+        <div class="Snd-child">11</div>
+        <div class="Snd-child">12</div>
+        <div class="Snd-child">13</div>
+        <div class="Snd-child">14</div>
+        <div class="Snd-child">15</div>
+      </div>
+      <div class="wrap-2">
+        <div class="Snd-child">1</div>
+        <div class="Snd-child">2</div>
+        <div class="Snd-child">3</div>
+        <div class="Snd-child">4</div>
+        <div class="Snd-child">5</div>
+        <div class="Snd-child">6</div>
+        <div class="Snd-child">7</div>
+        <div class="Snd-child">8</div>
+        <div class="Snd-child">9</div>
+        <div class="Snd-child">10</div>
+        <div class="Snd-child">11</div>
+        <div class="Snd-child">12</div>
+        <div class="Snd-child">13</div>
+        <div class="Snd-child">14</div>
+        <div class="Snd-child">15</div>
+      </div>
+      <div class="wrap-2">
+        <div class="Snd-child">1</div>
+        <div class="Snd-child">2</div>
+        <div class="Snd-child">3</div>
+        <div class="Snd-child">4</div>
+        <div class="Snd-child">5</div>
+        <div class="Snd-child">6</div>
+        <div class="Snd-child">7</div>
+        <div class="Snd-child">8</div>
+        <div class="Snd-child">9</div>
+        <div class="Snd-child">10</div>
+        <div class="Snd-child">11</div>
+        <div class="Snd-child">12</div>
+        <div class="Snd-child">13</div>
+        <div class="Snd-child">14</div>
+        <div class="Snd-child">15</div>
+      </div>
+      <div class="wrap-2">
+        <div class="Snd-child">1</div>
+        <div class="Snd-child">2</div>
+        <div class="Snd-child">3</div>
+        <div class="Snd-child">4</div>
+        <div class="Snd-child">5</div>
+        <div class="Snd-child">6</div>
+        <div class="Snd-child">7</div>
+        <div class="Snd-child">8</div>
+        <div class="Snd-child">9</div>
+        <div class="Snd-child">10</div>
+        <div class="Snd-child">11</div>
+        <div class="Snd-child">12</div>
+        <div class="Snd-child">13</div>
+        <div class="Snd-child">14</div>
+        <div class="Snd-child">15</div>
+      </div>
+    </div>
+    <div class="first-child">
+      <div class="wrap-2">
+        <div class="Snd-child">1</div>
+        <div class="Snd-child">2</div>
+        <div class="Snd-child">3</div>
+      </div>
+      <div class="wrap-2 wrap-3">
+        <div class="Snd-child">1</div>
+        <div class="Snd-child">2</div>
+        <div class="Snd-child">3</div>
+      </div>
+    </div>
+    <div class="first-child">
+      <div class="wrap-2 wrap-3">
+        <div class="Snd-child filed">1</div>
+        <div class="Snd-child filed">2</div>
+        <div class="Snd-child filed">3</div>
+      </div>
+    </div>
+    
+  </div>
 
 
 
-<script src="<?php // echo base_url('assets/js/main.js');?>"></script>
+
+<script src="<?php echo base_url('assets/js/main.js');?>"></script>
 </body>
 </html>
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,14 @@
-prompt("Whats Your Name?")
+var customerName = prompt("Please enter your name", "");
+
+if (customerName!= null){
+  document.getElementById("give").innerHTML =
+  "Hello " + customerName +  "! How are you today?";
+}
+
+// var r = confirm("Are you sure you want to send a Payment?");
+// if(r == true){
+//   x = "Payment Sent";
+// } else{
+//   x = "payment Cancelled!";
+// }
+// alert(x);
